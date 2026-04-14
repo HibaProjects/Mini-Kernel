f9e623a11e96f85f8682652a7f8158d628f46dbc
HibaProjects <hibaelorfi127@gmail.com>
2026-04-14 11:55:44 +0100

    Initial clean main (from scratch)

--- include/screen.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCREEN_H
-#define SCREEN_H
-
-void putstr(const char *str);
-void putstr_center(const char *str);
-
-#endif
--- kernel/kernel.c
+++
@@ -1,7 +0,0 @@
-#include "screen.h"
-
-void kernel_main() {
-    putstr_center("Welcome to MiniKernel\nLet's get started!");
-
-    while (1);
-}
